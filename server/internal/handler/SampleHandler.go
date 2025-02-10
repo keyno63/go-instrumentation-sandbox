960e96ec7206c9a4ec1ec7426ee9fdb6594dcf10
keyno63 <kofujiw@gmail.com>
2025-02-10 23:52:24 +0900

    add code

--- server/internal/handler/SampleHandler.go
+++ server/internal/handler/SampleHandler.go
@@ -5,6 +5,9 @@ import (
 	"net/http"
 )
 
+type SampleControllerInterface interface {
+}
+
 // SampleHandler is a simple HTTP handler function
 func SampleHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, this is a sample handler!")
